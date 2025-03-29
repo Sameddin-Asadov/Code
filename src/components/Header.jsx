5b729c27ae4cde53d33e80e5d2168572c83e37e2
Asadov8421 <sameddinasasdov8421@gmail.com>
2025-03-29 05:56:25 +0400

    Header Links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css'
 import logo from '../logo/CodeWorldLogo.png'
 import { GiHamburgerMenu } from "react-icons/gi";
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+
 function Header() {
+    const [open,setOpen]=useState(false)
+    const toggleDrawer = () => () => {
+        setOpen(newOpen);
+      };
+      // const links=["Ana Səifə","Haqqımızda","Kurslar","Əlaqə"]
+      const links= [
+        {title:"Ana Səifə",href:'#'},
+      {title:"Haqqımızda",href:'#'},
+      {title:"Kurslar",href:'#'},
+      {title:"Əlaqə",href:'#'}]
     return (
         <div className='header-menu'>
             <div className="header-left">
@@ -10,17 +23,19 @@ function Header() {
             </div>
             <div className='header-right'>
             <ul className='header-links'>
-                <li><a href="">Ana Səifə</a></li>
-                <li><a href="">Haqqımızda</a></li>
-                <li><a href="">Kurslar</a></li> 
-                <li><a href="">Əlaqə</a></li>
+              {links && links.map((link)=>(
+                <li key={link.title} ><a href={link.href}>{link.title}</a></li>
+              ))}
                 
             </ul>
-            <div className='mobile-menu'><GiHamburgerMenu style={{ 
+            <div className='mobile-menu'>
+      <Button onClick={toggleDrawer(true)}><GiHamburgerMenu style={{ 
         color: "#FFF",
         fontSize: "1.5em",
         cursor: "pointer"
-      }} /></div>
+      }} /></Button>
+<Drawer open={open} onClose={toggleDrawer(false)}>
+</Drawer></div>
             </div>
         </div>
     )
